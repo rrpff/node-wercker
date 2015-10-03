25cd83abc76f8d09273c4c8824049f8006494c64
Richard Foster <richard@madetech.co.uk>
2015-10-03 14:31:03 +0100

    Add proxy methods for info on applications, deploys and builds

--- src/index.js
+++ src/index.js
@@ -17,6 +17,30 @@ class Wercker extends API {
       },
     };
   }
+
+  applications (username) {
+    return this.get(`/applications/${username}`);
+  }
+
+  application (username, name) {
+    return this.get(`/applications/${username}/${name}`);
+  }
+
+  builds (username, name) {
+    return this.get(`/applications/${username}/${name}/builds`);
+  }
+
+  build (id) {
+    return this.get(`/builds/${id}`);
+  }
+
+  deploys (username, name) {
+    return this.get(`/applications/${username}/${name}/deploys`);
+  }
+
+  deploy (id) {
+    return this.get(`/deploys/${id}`);
+  }
 }
 
 module.exports = Wercker;
