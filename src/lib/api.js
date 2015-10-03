1ec358e020aa5af35b2b0cf53e601bc7cc34d9cc
Richard Foster <richard@madetech.co.uk>
2015-10-03 14:02:38 +0100

    Create basic low level API class

---
+++ src/lib/api.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const Path = require('path');
+
+class API {
+  static request (method, options = {}) {
+    options.method = method;
+
+    return new Promise(function (accept, reject) {
+      request(options, function (err, response, body) {
+        if (err || response.statusCode < 200 || response.statusCode > 299) {
+          reject(err);
+        } else {
+          try {
+            accept(JSON.parse(body));
+          } catch (e) {
+            reject(e)
+          }
+        }
+      });
+    });
+  }
+
+  defaults () {
+    return {};
+  }
+
+  host (resource) {
+    return resource;
+  }
+
+  request (method, resource, options = {}) {
+    let requestOptions = Object.assign({}, this.defaults(), options);
+    requestOptions.uri = this.host(resource);
+
+    return this.constructor.request(method, requestOptions);
+  }
+}
+
+['get', 'put', 'post', 'patch', 'delete'].forEach(method => {
+  API.prototype[method] = function () {
+    return this.request(method, ...arguments);
+  }
+});
+
+module.exports = API;
